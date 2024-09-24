d3cfafa32e239c5ebb9153ea63821074b6f00d6a
Christoffer <christoffer.isenberg@gmail.com>
2024-09-24 11:36:11 +0200

    Added BankAccountDto

---
+++ Bankomat/BankAccountDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankomat
+{
+    internal class BankAccountDto
+    {
+        public int AccountNumber { get; set; }
+        public int Balance { get; set; }
+
+
+        public BankAccount ToBankAccount()
+        {
+            return new BankAccount(AccountNumber, Balance);
+        }
+
+        public static BankAccountDto FromBankAccount(BankAccount bankAccount)
+        {
+            return new BankAccountDto { 
+                AccountNumber = bankAccount.AccountNumber, 
+                Balance = bankAccount.Balance
+            };
+        }
+    }
+}
